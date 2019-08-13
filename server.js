63195b650b342f25ae638332d846373a2f086409
William <wtpwinberg@gmail.com>
2019-08-12 19:53:19 -0600

    added server.js config file

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/vuenodedb').then(
+  () => { console.log('Database connection is successful') },
+  err => { console.log('Error when connecting to the database' + err) }
+)
+const app = express()
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(cors())
+
+const port = process.env.PORT || 4000
+
+app.listen(() => {
+  console.log('Listening on port ' + port)
+})
