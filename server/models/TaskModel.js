d6b6213b94bb995523b311b287e0e26351b28d60
William <wtpwinberg@gmail.com>
2019-08-16 13:42:39 -0600

    task model updated

--- server/models/TaskModel.js
+++ server/models/TaskModel.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose')
 const ObjectId = mongoose.Schema.Types.ObjectId
 
 const TaskSchema = mongoose.Schema({
+  site: {
+    type: String,
+    required: true
+  },
   name: {
     type: String,
     required: true
@@ -10,8 +14,44 @@ const TaskSchema = mongoose.Schema({
     type: String,
     required: true
   },
+  priority: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  points: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  assignee: [{
+    type: ObjectId,
+    ref: 'User'
+  }],
+  reporter: [{
+    type: ObjectId,
+    ref: 'User'
+  }],
+  comment: {
+    type: String,
+    required: true
+  },
+  dateAdded: {
+    type: mongoose.Schema.Types.Date,
+    default: Date.now
+  },
+  dateComplete: { type: mongoose.Schema.Types.Date,
+    default: null
+  },
   user: [{ type: ObjectId, ref: 'User' }],
-  board: [{ type: ObjectId, ref: 'Board' }]
+  board: [{ type: ObjectId, ref: 'Board' }],
+  column: [{ type: ObjectId, ref: 'Column' }]
 }, {
   timestamps: true
 })
