ba18626c8ce9a5a4dd4b3bcb84aa54c81c3b0229
William <wtpwinberg@gmail.com>
2019-08-16 00:28:52 -0600

    user profile, boards and tasks routes added

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -20,26 +20,23 @@ router
           const { board } = user
           const tasks = board.tasks
           User
-            .findOne({
-              topSkills: { $in: topSkills },
-              _id: { $not: { $in: [...skippedUsers, ...likedUsers] } }
-            }).select('-password -likedJobs -matchedJobs -skippedJobs -email')
-            .then((seeker) => {
-              res.status(200).json({ job, seeker })
+            .find({
+              tasks: { $in: tasks },
+              _id: { $not: { $in: [...archived, ...otherUsers] } }
+            }).select('-password -tasks -archived -other -email')
+            .then((user) => {
+              res.status(200).json({ tasks, user })
             })
         }).catch(err => res.status(500).json({ message: err.message }))
     })
   .post('/register', (req, res) => {
     const data = decode(req.body.token)
-
     data.forEach(field => {
       if (!field) {
         return res.status(300).json({ message: `${field} is required` })
       }
     })
-
     const user = new User(data)
-
     user
       .save()
       .then((profile) => {
@@ -80,6 +77,74 @@ router
           .catch(err => res.status(500).json(err))
       })
       .catch(err => res.status(500).json(err))
+  })
+  .put('/task', passport.authenticate('bearer'), (req, res) => {
+    const user = req.user
+    const { type } = req.user
+    const { userId } = req.params
+    const { taskId, superLike, skip } = req.body
+    User
+      .findById(userId)
+      .then((user) => {
+        Task
+          .findById(taskId)
+          .then((task) => {
+            const { board, tasks } = user
+            const { id, title, description } = task
+            task
+              .save()
+              .then(() => {
+                user
+                  .update({ board })
+                  .then(() => {
+                    user
+                      .update({ tasks })
+                      .then(() => {
+                        res.status(200).json({ tasks })
+                      }).catch(err => res.status(500).json({ at: 'User update', message: err.message }))
+                  }).catch(err => res.status(500).json({ at: 'User update', message: err.message }))
+              }).catch(err => res.status(500).json({ at: 'Task update', message: err.message }))
+          }).catch(err => res.status(500).json({ at: 'Find task', message: err.message }))
+      }).catch(err => res.status(500).json({ at: 'Find user', message: err.message }))
+  })
+  .get('/boards', passport.authenticate('bearer'), (req, res) => {
+    const { type, boards } = req.user
+    if (type !== 'user') {
+      return res.status(400).json({ message: 'Must be logged in to view boards.' })
+    }
+    Board.find({ _id: ownedBoards, isActive: true })
+      .select('titleAndSalary').populate('archivedUsers')
+      .then((boards) => {
+        res.status(200).json(boards)
+      })
+      .catch((err) => {
+        res.status(500).json({ message: err.message })
+      })
+  })
+  .get('/profile', passport.authenticate('bearer', { session: false }),
+    (req, res) => {
+      res.status(200).json(req.user)
+    })
+  .put('/profile', passport.authenticate('bearer', { session: false }), (req, res) => {
+    const oldUser = req.user
+    const buffer = Object.keys(req.body)
+    const restricted = ['type', 'tasks', 'password']
+    const changes = {}
+    buffer.forEach((key) => {
+      if (!restricted.includes(key)) {
+        if (req.body[key]) {
+          changes[key] = req.body[key]
+        }
+      }
+    })
+    const updatedUser = Object.assign(oldUser, changes)
+    updatedUser
+      .validate()
+      .then(() => {
+        User.findOneAndUpdate({ email: oldUser.email }, changes).then(() => {
+          res.status(200).json(changes)
+        }).catch(err => res.status(500).json({ message: err.message }))
+      }).catch(err => res.status(322).json({ message: err.message }))
   })
 
 
