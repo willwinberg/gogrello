31b95ff64dd012b121d6465bc08ec2e16329898a
William <wtpwinberg@gmail.com>
2019-08-23 12:37:16 -0600

    router to db working properly now

--- server/server.js
+++ server/server.js
@@ -1,19 +1,18 @@
 const express = require('express')
-const path = require('path')
+// const path = require('path')
 const cors = require('cors')
 const helmet = require('helmet')
-const passport = require('passport')
+// const passport = require('passport')
 const morgan = require('morgan')
-const strategies = require('./strategies')
+// const strategies = require('./strategies')
 const mongoose = require('mongoose')
-
 const taskRouter = require('./routers/taskRouter.js')
 const userRouter = require('./routers/userRouter.js')
-const { userExists } = require('./routers/helpers')
+// const { userExists } = require('./routers/helpers')
 var birds = require('./routers/birds')
 
-mongoose.connect('mongodb://localhost:27017/gogrellodb', {useNewUrlParser: true}).then(
-  () => { console.log('Database connection is successful') },
+mongoose.connect('mongodb://localhost:27017/gogrellodb', { useNewUrlParser: true }).then(
+  () => { debugger; console.log('Database connection is successful') },
   err => { console.log('Error when connecting to the database' + err) }
 )
 
@@ -31,18 +30,26 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
 app.use(helmet())
-app.use(passport.initialize())
-app.use(passport.session())
+// app.use(passport.initialize())
+// app.use(passport.session())
+// strategies()
 
-strategies()
+const User = require('./models/userModel')
+console.log(User)
 
-// app.get('/', function (req, res) {
-//   res.json({ api: 'Running...' })
-// })
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/:id', (req, res) => {
+  User
+    .findById(req.params.id)
+    .then(response => {
+      console.log('fount')
+      res.status(200).json(response)
+    })
+    .catch(err => res.status(500).json({ message: err.message }))
+})
+
 app.use('/api/users', userRouter)
 app.use('/api/tasks', taskRouter)
-app.post('/api/exists', userExists)
+// app.post('/api/exists', userExists)
 app.use('/birds', birds)
 
 const port = process.env.PORT || 3000
